bc476fbd65fb37a5ed4c92236dd51da34f5724b7
thebrandonhoward <thebrandonhoward@outlook.com>
2025-01-03 15:38:04 -0600

    Adds refactorings

--- notes.py
+++ notes.py
@@ -1,36 +1,45 @@
 import os
 import subprocess
-import utilz
+from utilz import adddescription, addspacer, addline
 
 """
 Random Python Notes
 """
 #--------------------------------------------------------------------------------------------------
-utilz.adddescription("Call a startup script:")
-os.system('python3 startup.py')
-#--------------------------------------------------------------------------------------------------
-utilz.adddescription("Read file and print the contents of this script:")
-thisFile = str(__file__)
+def printNotes():
+    adddescription("Call a startup script:")
+    os.system('python3 startup.py')
+    addline()
+    #--------------------------------------------------------------------------------------------------
+    adddescription("Read file and print the contents of this script:")
+    thisFile = str(__file__)
 
-file = None
+    file = None
 
-try:
-    file = open(thisFile)
+    try:
+        file = open(thisFile)
 
-    for f in file:
-        print(str(f))
+        for f in file:
+            print(str(f))
 
-except FileNotFoundError:
-    print("%s %s" % (thisFile, "does not exist."))
-finally:
-    if str(type(file)) == "<class '_io.TextIOWrapper'>":
-        file.close()
-        print("<<FILE SUCCESSFULLY CLOSED>>")
-#--------------------------------------------------------------------------------------------------
-utilz.adddescription("Multiply with string prints n times")
-print("%s" % ("he" * 10))
-#--------------------------------------------------------------------------------------------------
-utilz.adddescription("Run shell commands")
-print(subprocess.run(["ls", "-larit"], capture_output=True, text=True).stdout)
-print(subprocess.run(["python3", "greeting.py"], capture_output=True, text=True).stdout)
-#--------------------------------------------------------------------------------------------------
+    except FileNotFoundError:
+        print("%s %s" % (thisFile, "does not exist."))
+    finally:
+        if str(type(file)) == "<class '_io.TextIOWrapper'>":
+            file.close()
+            print("<<FILE SUCCESSFULLY CLOSED>>")
+
+    addline()
+    #--------------------------------------------------------------------------------------------------
+    adddescription("Multiply with string prints n times")
+    print("%s" % ("he" * 10))
+    addline()
+    #--------------------------------------------------------------------------------------------------
+    adddescription("Run shell commands")
+    print(subprocess.run(["ls", "-larit"], capture_output=True, text=True).stdout)
+    print(subprocess.run(["python3", "greeting.py"], capture_output=True, text=True).stdout)
+    addline()
+
+# -Main -------------------------------------------------------------------------------------------
+printNotes()
+
